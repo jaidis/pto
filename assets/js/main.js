6105d783a8cbebbf16eb78737d5139b7bf0582e3
Manuel Muñoz <heidiaricel@hotmail.com>
2018-05-05 13:19:21 +0200

    Installing assets for jVectorMap 2.0.3
    Installing assets for jVectorMap 2.0.3 from http://jvectormap.com/

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     // hide our element on page load
-    $('.h1-jumbo').css('opacity', 0);
+    // $('.h1-jumbo').css('opacity', 0);
 
     $('.h1-jumbo').waypoint(function() {
         $('.h1-jumbo').addClass('fadeInDown');
-    }, { offset: '50%' });
+    }, { offset: '35%' });
 
     // hide our element on page load
     $('#p-jumbo').css('opacity', 0);
@@ -12,4 +12,22 @@ $(document).ready(function () {
     $('#p-jumbo').waypoint(function() {
         $('#p-jumbo').addClass('fadeInUp');
     }, { offset: '65%' });
-})
+
+    $('#mapa').vectorMap({
+        map: 'es_merc',
+        backgroundColor: '#efffff',
+        regionsSelectable: true,
+        regionStyle: {
+            initial: {
+                fill: '#18bc9c'
+            },
+            selected: {
+                fill: '#ffec00'
+            }
+        },
+        onRegionClick: function (event, code) {
+            console.log(event, code);
+            // window.location.replace("http://your url address/"+code+"");
+        }
+    });
+});
