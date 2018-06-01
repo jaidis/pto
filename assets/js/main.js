df521c4af55f354585c1492af3ff1f60fce78ce8
Manuel Muñoz <heidiaricel@hotmail.com>
2018-06-01 20:16:23 +0200

    Added Normalize.css
    - Added Normalize.css library from http://necolas.github.io/normalize.css/
    
    - Created Toastr template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+function toastrTemplate (type, message){
+    toastr[type](message).options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "300",
+        "timeOut": "10000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "swing",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+}
+
 $(document).ready(function () {
 
     $('.h1-jumbo').waypoint(function() {
