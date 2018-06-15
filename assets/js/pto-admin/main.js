a146fc1d74902b7aad2c352ff24dfe3397003712
Manuel Muñoz <heidiaricel@hotmail.com>
2018-06-15 12:45:41 +0200

    Configure 'ModularAdmin' assets
    - Configure 'ModularAdmin' assets
    - Installed 'datatables' assets, more info at https://www.datatables.net/

---
+++ assets/js/pto-admin/main.js
@@ -0,0 +1,19 @@
+function lanzarToast (type, message){
+    toastr[type](message).options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": true,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": true,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "300",
+        "timeOut": "10000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "swing",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+}
